5f8d067838880c6f7bee569b5ee50a52946d9db7
Tony Creavin <tonycreavin@gmail.com>
2023-02-15 12:42:24 +0100

    initial commit and routes added

---
+++ src/pages/Freelances.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Freelances() {
+  return <div>Freelances</div>;
+}
+
+export default Freelances;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import '../../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">MyApp</header>
+    </div>
+  );
+}
+
+export default Home;
